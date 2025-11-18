465c04d2d74d4f8907b38b07a5b3565f48edcc8f
lolochno <lolochno@gmail.com>
2025-11-18 14:32:21 +0300

    3-ий коммит

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 class Main{
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
+        int a = 0;
+        for (int i = 0; i < 10; i++) {
+            System.out.println(a++);
+        }
     }
 }

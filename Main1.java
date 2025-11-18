d57400f378679a5ddd964e29276b4bcb40b3c030
lolochno <lolochno@gmail.com>
2025-11-18 15:28:07 +0300

    11-ый коммит

--- Main1.java
+++ Main1.java
@@ -1,7 +1,10 @@
 public class Main1 {
     public static void main(String[] args) {
         System.out.println("Bye-bye!");
+
+        int a = 10;
+        int b = 10;
+
+        System.out.println("abcdefdg");
     }
-
-
 }

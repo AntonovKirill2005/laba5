216be1e0994499c64e2e89d48fd3836e4bf17cf0
lolochno <lolochno@gmail.com>
2025-11-18 15:26:17 +0300

    10-ый коммит

--- Tools.java
+++ Tools.java
@@ -1,3 +1,9 @@
 public class Tools {
-
+    public void equal(int a, int b){
+        if (a == b) {
+            System.out.println("РАВНО!");
+        } else{
+            System.out.println("Неравно");
+        }
+    }
 }
